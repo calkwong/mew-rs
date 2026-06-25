0d47a5d32d808ee195fbee582fa656208c3aecad
calkwong <75272424+calkwong@users.noreply.github.com>
2026-06-25 16:52:24 +0800

    validation layers

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,91 @@
 use ash::{Entry, Instance, ext::debug_utils, vk};
 // use ash_window;
 
-use std::ffi::c_char;
+use std::{borrow::Cow, ffi, os::raw::c_char};
 
 fn main() {
-    let entry = Entry::linked();
-
-    let app_name = c"mew-rust";
-
-    let layer_names = [c"VK_LAYER_KHRONOS_validation"];
-    let layers_names_raw: Vec<*const c_char> = layer_names
-        .iter()
-        .map(|raw_name| raw_name.as_ptr())
-        .collect();
-
-    let mut extension_names: Vec<*const c_char> = Vec::new();
-    extension_names.push(debug_utils::NAME.as_ptr());
-
-    let app_info = vk::ApplicationInfo::default()
-        .application_name(app_name)
-        .application_version(0)
-        .engine_name(app_name)
-        .engine_version(0)
-        .api_version(vk::make_api_version(0, 1, 3, 0));
-
-    let instance_create_info = vk::InstanceCreateInfo::default()
-        .application_info(&app_info)
-        .enabled_layer_names(&layers_names_raw)
-        .enabled_extension_names(&extension_names)
-        .flags(vk::InstanceCreateFlags::default());
-
     unsafe {
+        let entry = Entry::linked();
+
+        let app_name = c"mew-rust";
+
+        let layer_names = [c"VK_LAYER_KHRONOS_validation"];
+        let layers_names_raw: Vec<*const c_char> = layer_names
+            .iter()
+            .map(|raw_name| raw_name.as_ptr())
+            .collect();
+
+        let mut extension_names: Vec<*const c_char> = Vec::new();
+        extension_names.push(debug_utils::NAME.as_ptr());
+
+        let app_info = vk::ApplicationInfo::default()
+            .application_name(app_name)
+            .application_version(0)
+            .engine_name(app_name)
+            .engine_version(0)
+            .api_version(vk::make_api_version(0, 1, 3, 0));
+
+        let instance_create_info = vk::InstanceCreateInfo::default()
+            .application_info(&app_info)
+            .enabled_layer_names(&layers_names_raw)
+            .enabled_extension_names(&extension_names)
+            .flags(vk::InstanceCreateFlags::default());
+
         let instance: Instance = entry
             .create_instance(&instance_create_info, None)
             .expect("Instance creation failed");
 
+        let debug_info = vk::DebugUtilsMessengerCreateInfoEXT::default()
+            .message_severity(
+                vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::INFO,
+            )
+            .message_type(
+                vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                    | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                    | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+            )
+            .pfn_user_callback(Some(vulkan_debug_callback));
+
+        let debug_utils_loader = debug_utils::Instance::new(&entry, &instance);
+        let debug_call_back = debug_utils_loader
+            .create_debug_utils_messenger(&debug_info, None)
+            .unwrap();
+
         // destroys
+        debug_utils_loader.destroy_debug_utils_messenger(debug_call_back, None);
         instance.destroy_instance(None);
     }
+}
+
+unsafe extern "system" fn vulkan_debug_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT<'_>,
+    _user_data: *mut std::os::raw::c_void,
+) -> vk::Bool32 {
+    let callback_data;
+    unsafe {
+        callback_data = *p_callback_data;
+    }
+    let message_id_number = callback_data.message_id_number;
+
+    let message_id_name = if callback_data.p_message_id_name.is_null() {
+        Cow::from("")
+    } else {
+        unsafe { ffi::CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy() }
+    };
+
+    let message = if callback_data.p_message.is_null() {
+        Cow::from("")
+    } else {
+        unsafe { ffi::CStr::from_ptr(callback_data.p_message).to_string_lossy() }
+    };
+
+    println!(
+        "{message_severity:?}:\n{message_type:?} [{message_id_name} {message_id_number}] : {message}\n",
+    );
+
+    vk::FALSE
 }
