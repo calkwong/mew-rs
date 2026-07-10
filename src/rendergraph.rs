038793c20d762a9d9b45826b0dcfde514d6a35f7
calkwong <75272424+calkwong@users.noreply.github.com>
2026-07-11 01:02:56 +0800

    work on callback execution fn

--- src/rendergraph.rs
+++ src/rendergraph.rs
@@ -143,8 +143,10 @@ pub enum RenderPass {
 pub struct Pass<'a, T> {
     reads: Vec<&'a str>,
     writes: Vec<&'a str>,
+    pipeline: vk::Pipeline,
     constants: &'a [u8],
-    // TODO: callback fn, and possibly pipeline handle?
+    // TODO: Fn or FnOnce?
+    execute: Box<dyn Fn(ash::Device, vk::CommandBuffer, vk::PipelineLayout) + 'a>,
     render_pass: RenderPass,
     _marker: PhantomData<T>,
 }
@@ -168,6 +170,11 @@ impl<'a, T> Pass<'a, T> {
         self.constants = data;
         self
     }
+
+    pub fn pipeline(mut self, pipeline: vk::Pipeline) -> Self {
+        self.pipeline = pipeline;
+        self
+    }
 }
 
 // TODO: dispatch_indirect variant
@@ -178,24 +185,31 @@ pub struct ComputePass {
 }
 
 impl<'a> Pass<'a, ComputePass> {
+    // TODO: can we make this a default?
     pub fn new_compute() -> Self {
         Self {
             reads: Vec::new(),
             writes: Vec::new(),
+            pipeline: vk::Pipeline::null(),
             constants: &[],
+            execute: Box::new(|_, _, _| {}),
             render_pass: RenderPass::Compute(ComputePass { x: 0, y: 0, z: 0 }),
             _marker: PhantomData,
         }
     }
 
     pub fn dispatch(mut self, x: u32, y: u32, z: u32) -> Self {
-        if let RenderPass::Compute(data) = &mut self.render_pass {
-            data.x = x;
-            data.y = y;
-            data.z = z;
-        } else {
-            panic!("Dispatch called on wrong render pass type");
+        match self.render_pass {
+            RenderPass::Compute(_) => {}
+            _ => panic!("Dispatch called on wrong render pass type"),
         }
+
+        self.execute = Box::new(move |device, cmd, layout| unsafe {
+            device.cmd_bind_pipeline(cmd, vk::PipelineBindPoint::COMPUTE, self.pipeline);
+            device.cmd_push_constants(cmd, layout, vk::ShaderStageFlags::ALL, 0, self.constants);
+            device.cmd_dispatch(cmd, x, y, z);
+        });
+
         self
     }
 }
@@ -213,11 +227,14 @@ pub struct GraphicsPass {
 }
 
 impl<'a> Pass<'a, GraphicsPass> {
+    // TODO: can we make this a default?
     pub fn new_graphics() -> Self {
         Self {
             reads: Vec::new(),
             writes: Vec::new(),
+            pipeline: vk::Pipeline::null(),
             constants: &[],
+            execute: Box::new(|_, _, _| {}),
             render_pass: RenderPass::Graphics(GraphicsPass {
                 render_targets: Vec::new(),
                 depth_target: None,
@@ -251,15 +268,104 @@ impl<'a> Pass<'a, GraphicsPass> {
         self
     }
 
-    pub fn custom<F>(mut self, f: F) -> Self
-    where
-        F: Fn() + 'static,
-    {
+    pub fn draw_indirect(
+        mut self,
+        buffer: vk::Buffer,
+        offset: vk::DeviceSize,
+        count_buffer: vk::Buffer,
+        count_buffer_offset: vk::DeviceSize,
+        max_draw_count: u32,
+        stride: u32,
+        index_buffer: vk::Buffer,
+        extent: vk::Extent2D,
+    ) -> Self {
+        let mut color_attachments: Vec<vk::RenderingAttachmentInfo> = Vec::new();
+        let mut depth_attachment: Option<vk::RenderingAttachmentInfo> = None;
         if let RenderPass::Graphics(data) = &mut self.render_pass {
-            data.custom = Box::new(f);
-        } else {
-            panic!("Custom called on wrong render pass type");
+            color_attachments = data
+                .render_targets
+                .iter()
+                .map(|target| {
+                    let mut attachment = vk::RenderingAttachmentInfo::default()
+                        .image_view(target.view)
+                        .image_layout(vk::ImageLayout::GENERAL)
+                        .load_op(target.load_op)
+                        .store_op(vk::AttachmentStoreOp::STORE);
+
+                    if target.load_op == vk::AttachmentLoadOp::CLEAR {
+                        attachment = attachment.clear_value(vk::ClearValue {
+                            color: vk::ClearColorValue {
+                                float32: [0.0, 0.0, 0.0, 1.0],
+                            },
+                        });
+                    }
+
+                    attachment
+                })
+                .collect();
+
+            if let Some(depth_target) = &data.depth_target {
+                let mut attachment = vk::RenderingAttachmentInfo::default()
+                    .image_view(depth_target.view)
+                    .image_layout(vk::ImageLayout::GENERAL)
+                    .load_op(depth_target.load_op)
+                    .store_op(vk::AttachmentStoreOp::STORE);
+
+                if depth_target.load_op == vk::AttachmentLoadOp::CLEAR {
+                    attachment = attachment.clear_value(vk::ClearValue {
+                        depth_stencil: vk::ClearDepthStencilValue {
+                            depth: 0.0,
+                            stencil: 0,
+                        },
+                    });
+                }
+
+                depth_attachment = Some(attachment);
+            }
         }
+
+        let viewport = [vk::Viewport::default()
+            .y(extent.height as f32)
+            .width(extent.width as f32)
+            .height(-(extent.height as f32))
+            .max_depth(1.0)
+            .min_depth(0.0)];
+
+        let scissors = [vk::Rect2D {
+            extent,
+            ..Default::default()
+        }];
+
+        self.execute = Box::new(move |device, cmd, layout| unsafe {
+            let mut rendering_info = vk::RenderingInfo::default()
+                .render_area(vk::Rect2D {
+                    extent,
+                    ..Default::default()
+                })
+                .color_attachments(&color_attachments)
+                .layer_count(1);
+
+            if let Some(ref depth_attachment) = depth_attachment {
+                rendering_info = rendering_info.depth_attachment(depth_attachment);
+            }
+
+            device.cmd_bind_pipeline(cmd, vk::PipelineBindPoint::GRAPHICS, self.pipeline);
+            device.cmd_set_viewport(cmd, 0, &viewport);
+            device.cmd_set_scissor(cmd, 0, &scissors);
+            device.cmd_begin_rendering(cmd, &rendering_info);
+            device.cmd_push_constants(cmd, layout, vk::ShaderStageFlags::ALL, 0, self.constants);
+            device.cmd_bind_index_buffer(cmd, index_buffer, 0, vk::IndexType::UINT32);
+            device.cmd_draw_indexed_indirect_count(
+                cmd,
+                buffer,
+                offset,
+                count_buffer,
+                count_buffer_offset,
+                max_draw_count,
+                stride,
+            );
+        });
+
         self
     }
 }
